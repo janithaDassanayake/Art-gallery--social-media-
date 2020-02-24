859da37607773e2ded49c34248bf3f9dbd9523a6
janithaDassanayake <janithaprathapa@yahoo.com>
2020-02-24 23:16:42 +0530

    post option added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,21 +9,20 @@
         </div>
 
         <div class="row">
-            <div class="col-9 pt-5" >
-                <div><h1>janithArts</h1></div>
+            <div class="col-9 pt-4" >
+                <div><h1>{{$user->username}}</h1></div>
                 <div class="d-flex">
                 <div class="pr-5"><strong>144</strong>posts</div>
                 <div class="pr-5"><strong>24k</strong>followers</div>
                 <div class="pr-5"><strong>321</strong>following</div>
                 </div>
                 
-                <div class="pt-4 font-weight-bold">JanithArts.org</div>
+                <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
                 <div class="pt-2">
-                “You are an artist, no matter how often you make 
-                art or how much you make from it.”
+                {{$user->profile->bio}}
                 </div>
 
-                <div class="pt-2 font-weight-bold"> <a href="#"> www.SelfTaughtArtist.org</a></div>
+                <div class="pt-2 font-weight-bold"> <a href="#"> {{$user->profile->url }}</a></div>
             </div>
         </div>
     </div>
@@ -33,8 +32,6 @@
         <div class="col-4">
         <img src="https://instagram.fcmb2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.75.896.896a/s640x640/26184751_469402546790259_5297139240973369344_n.jpg?_nc_ht=instagram.fcmb2-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=MKMB-bvAz_UAX_B6sTE&oh=11789a981e1b3ed355713c0aff17f89d&oe=5E8595A2" class="w-100">
         </div>
-
-
 
         <div class="col-4">
         <img src="https://instagram.fcmb2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.104.1080.1080a/s640x640/28151851_2011471502437990_3126767652123443200_n.jpg?_nc_ht=instagram.fcmb2-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=rZIGx-UK_gMAX_FcFkw&oh=fd5f1273dc537fdaa074ec3ebd4a65c0&oe=5E821A5C" class="w-100">
